04918eb6ce1dc79a35ec4fbd4ecc18a32a8a3100
Renzo <renzoot21@gmail.com>
2026-01-16 09:07:30 -0500

    primera version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "com.validora.app"
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
     }
